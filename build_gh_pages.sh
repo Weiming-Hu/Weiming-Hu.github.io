4622e5628a68417d92c8c71e957387276c284349
Weiming Hu <Weiming-Hu@users.noreply.github.com>
2025-08-06 21:54:02 -0400

    add echo

--- build_gh_pages.sh
+++ build_gh_pages.sh
@@ -8,12 +8,19 @@ dx bundle --platform web
 
 git checkout gh-pages
 rm -rf assets index.html wasm
+
+echo Copy web files to gh-pages branch...
 cp -r target/dx/personal_website/release/web/public/* .
+
+echo Copying index.html to 404.html...
 cp index.html 404.html
+
+echo Committing changes to gh-pages branch...
 git add -A
 git commit -m "web release for version $VERSION"
 git push
 
+echo Cleaning up master branch...
 git checkout master
 [ -d wasm ] && rm -rf wasm
 [ -d 404.html ] && rm -rf 404.html
