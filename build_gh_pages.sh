bca720eabf8f309048abfc9c32f17ecbb239db51
Weiming Hu <Weiming-Hu@users.noreply.github.com>
2025-08-06 21:53:18 -0400

    trying to load urls

--- build_gh_pages.sh
+++ build_gh_pages.sh
@@ -9,11 +9,13 @@ dx bundle --platform web
 git checkout gh-pages
 rm -rf assets index.html wasm
 cp -r target/dx/personal_website/release/web/public/* .
+cp index.html 404.html
 git add -A
 git commit -m "web release for version $VERSION"
 git push
 
 git checkout master
 [ -d wasm ] && rm -rf wasm
+[ -d 404.html ] && rm -rf 404.html
 
 echo gh-pages have been updated to $VERSION!
